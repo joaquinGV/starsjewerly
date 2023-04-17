fd71f3e8fac70ad2e3cfceba31e6c774d63b7159
Joaquin Gonzalez <joaquinagv99@hotmail.com>
2023-04-17 15:25:40 -0600

    Juego piedra papel y tijera finalizado

---
+++ Clase 2/main.js
@@ -0,0 +1,47 @@
+let variableBoleana;
+
+variableBoleana = true;
+// variableBoleana = false;
+
+/// Sintaxis condition If
+
+if (variableBoleana) {
+  console.log("Este codigo se ve por que variableBoleana es True");
+}
+
+console.log("Resto del codigo");
+
+// Ejemplo Logeo de usuario
+
+let nombreUsuario;
+let contraseña;
+
+nombreUsuario = prompt("Ingrese su contraseña: ");
+if (contraseña !== 1) {
+  console.log("Contraseña Correcta!");
+}
+
+let numerito1 = 10;
+let numerito2 = 12;
+
+resultado = numerito1 > numerito2;
+console.log(numerito1 + " > " + numerito2 + " " + resultado);
+
+let edad;
+
+// edad = prompt("Ingrese su edad");
+// if (edad >= 18) {
+//   console.log("Ingresando a bebidas alcoholicas");
+// }
+
+/// != !===
+// numerito1 = 7;
+// numerito2 = "7";
+
+// // distinto en valor
+// resultado = numerito1 != numerito2;
+// console.log(numerito1 + " != " + numerito2 + " = " + resultado);
+
+// // Estrictamente distinto valor y tipo
+// resultado = numerito1 !== numerito2;
+// console.log(numerito1 + " !== " + numerito2 + " = " + resultado);
