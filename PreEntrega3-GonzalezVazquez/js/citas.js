2309328f10801795de3ad1b3fc7315997edaf142
Joaquin Gonzalez <joaquinagv99@hotmail.com>
2023-05-25 17:44:51 -0600

    inicio sesion empezado

--- PreEntrega3-GonzalezVazquez/js/citas.js
+++ PreEntrega3-GonzalezVazquez/js/citas.js
@@ -249,3 +249,51 @@ function horasDisponibles(date) {
 
 // Funciones que se ejecutan al cargar la aplicacion
 MostrarServicios();
+
+const botonInicio = document.getElementById("iniciarSesion");
+
+let nombreUsuario = null;
+let pass = null;
+
+botonInicio.onclick = () => {
+  Swal.fire({
+    title: "Login",
+    text: "Ingrese su mail de login",
+    input: "email",
+    inputPlaceholder: "Pepe@pepe.com",
+    confirmButtonText: "Enviar",
+    showCancelButton: true,
+    cancelButtonText: "Cancelar",
+  }).then((resultado) => {
+    ///una vez que el usuario ingreso el valro en el prompt y apreto algun boton
+    console.log(resultado);
+    if (resultado.isConfirmed) {
+      //apreto el boton enviar?
+      nombreUsuario = resultado.value; ///me llevo el valor del input
+      Swal.fire({
+        title: "Password",
+        text: "Ingrese su password",
+        input: "password",
+        confirmButtonText: "Enviar",
+        showCancelButton: true,
+        cancelButtonText: "Me arrepenti",
+      }).then((resultado) => {
+        if (resultado.isConfirmed) {
+          pass = resultado.value;
+          if (pass === "") {
+            ///si el password esta vacio
+            Swal.fire({
+              title: "El pass es invalido",
+              icon: "error",
+            });
+          } else {
+            Swal.fire({
+              title: "Ingreso de usuario Exitoso!",
+              icon: "success",
+            });
+          }
+        }
+      });
+    }
+  });
+};
